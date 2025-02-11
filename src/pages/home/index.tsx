947550b81c226e6ea274414d9e7f7ad3252103f0
YanGraim <yangraim@gmail.com>
2025-02-11 15:24:03 -0300

    consome a api na page de home utilizando hooks useState e useEffect

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,23 +1,50 @@
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
+
 import { BsCartPlus } from "react-icons/bs";
 
+interface ProductProps{
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    cover: string;
+}
+
 export function Home() {
+    const [products, setProducts] = useState<ProductProps[]>([]);
+
+    useEffect(() => {
+        async function getProducts() {
+            const response = await api.get("/products");
+            setProducts(response.data);
+        }
+
+        getProducts();
+    }, [])
+
     return (
         <div>
             <main className="w-full max-w-7xl px-4 mx-auto">
                 <h1 className="font-bold text-3xl mb-4 mt-10 text-center">Produtos em alta</h1>
                 <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-5">
-                    <section className="w-full">
-                        <img className="w-full rounded-lg max-h-70 mb-2" src="https://imgs.search.brave.com/y656VHqgkflw7lfr0_1psnmpO9AIMpCyic-oAqKjWEo/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9iZmFz/c2V0LmNvc3Rjby1z/dGF0aWMuY29tL1U0/NDdJSDM1L2FzL25q/M25oNG04YnJ0NnQ3/cjJodHRoY3R3LzEw/MDcxMzIxMi04NDdf/XzE_YXV0bz13ZWJw/JmZvcm1hdD1qcGcm/d2lkdGg9MzUwJmhl/aWdodD0zNTAmZml0/PWJvdW5kcyZjYW52/YXM9MzUwLDM1MA.jpeg" alt="imagem do produto" />
-                        <p className="font-medium mt-1 mb-2">Macbook Air M2</p>
-                        <div className="flex gap-3 items-center">
-                            <strong className="text-zinc-700/90">
-                                R$ 5000,00
-                            </strong>
-                            <button className="bg-zinc-900 p-1 rounded">
-                                <BsCartPlus size={20} color="#fff"/>
-                            </button>
-                        </div>
-                    </section>
+                    {products.map((product) => (
+                        <section className="w-full" key={product.id}>
+                            <img className="w-full rounded-lg max-h-70 mb-2" src={product.cover}  alt={product.title}/>
+                            <p className="font-medium mt-1 mb-2">{product.title}</p>
+                            <div className="flex gap-3 items-center">
+                                <strong className="text-zinc-700/90">
+                                    {product.price.toLocaleString("pt-BR", {
+                                        style: "currency",
+                                        currency: "BRL"
+                                    })}
+                                </strong>
+                                <button className="bg-zinc-900 p-1 rounded">
+                                    <BsCartPlus size={20} color="#fff"/>
+                                </button>
+                            </div>
+                        </section>
+                    ))}
                 </div>
             </main>
         </div>
