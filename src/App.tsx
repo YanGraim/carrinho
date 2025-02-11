dc0c10439566b8e734c6ccd1348b0fa1b5265bba
YanGraim <yangraim@gmail.com>
2025-02-11 11:11:58 -0300

    configura rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
-
 import { Home } from "./pages/home";
 import { Cart } from "./pages/cart";
+import { Layuot } from "./components/layout";
 
 const router = createBrowserRouter([
   {
-    
+    element: <Layuot/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/cart",
+        element: <Cart/>
+      }
+    ]
   }
-])
+]);
 
 export { router }
