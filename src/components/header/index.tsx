bee50064694183f49b7e244673364fb7efc18713
YanGraim <yangraim@gmail.com>
2025-02-11 11:28:28 -0300

    estiliza o component Header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,14 +3,17 @@ import { FiShoppingCart } from "react-icons/fi";
 
 export function Header() {
     return (
-        <header>
-            <nav>
-                <Link to={"/"}>
+        <header className="w-full px-2 bg-slate-300">
+            <nav className="w-full max-w-7xl h-14 flex items-center justify-between px-5 mx-auto">
+                <Link className="font-bold text-2xl italic" to={"/"}>
                     Dev Shop
                 </Link>
 
-                <Link to={"/cart"}>
+                <Link className="relative" to={"/cart"}>
                     <FiShoppingCart size={24} color="#121212"/>
+                    <span className="absolute -top-3 -right-3 px-2.5 bg-sky-500 rounded-full w-6 h-6 flex items-center justify-center text-white text-xs">
+                        2
+                    </span>
                 </Link>
             </nav>
         </header>
