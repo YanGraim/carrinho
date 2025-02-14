c884d1ffdb625f9d5313bd9e2b2c88854cfc0115
YanGraim <yangraim@gmail.com>
2025-02-14 10:57:41 -0300

    utiliza react-hot-toast

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,16 @@ import './index.css';
 import { router } from './App';
 import { RouterProvider } from 'react-router-dom';
 import CartProvider from './contexts/CartContext';
+import { Toaster } from 'react-hot-toast';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <CartProvider>
+      <Toaster
+        position="top-center"
+        reverseOrder={false}
+      />
       <RouterProvider router={router} />
     </CartProvider>
-  </StrictMode>,
+  </StrictMode>
 )
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,6 +3,7 @@ import { api } from "../../services/api";
 import { CartContext } from "../../contexts/CartContext";
 
 import { BsCartPlus } from "react-icons/bs";
+import toast from "react-hot-toast";
 
 export interface ProductProps{
     id: number;
@@ -26,6 +27,13 @@ export function Home() {
     }, [])
 
     function handleAddCart(product: ProductProps) {
+        toast.success("Produto adicionado com sucesso!", {
+            style: {
+                borderRadius: 10,
+                backgroundColor: "#121212",
+                color: "#FFF"
+            }
+        })
         addItemCart(product);
     }
     
